c0bee117656bdd98e8f5e76eb290b2dd47a7069c
Matt SS <hippiesa3@gmail.com>
2016-03-02 12:33:21 +0100

    Added new login
    Added login from different project

--- Urban/actions/login.php
+++ Urban/actions/login.php
@@ -1,6 +1,7 @@
 <?php
-include("inc/db.php");
-include ("inc/checked_logged_in.php");
+
+include("../actions/db.php");
+include ("../actions/checked_logged_in.php");
 
 $email = $_POST["email"];
 $pass = sha1($_POST["password"]);
@@ -13,6 +14,8 @@ if ($user = $stmt->fetchObject()) {
     $_SESSION["right_id"] = $user->right_id;
     $_SESSION["email"] = $user->email;
     $_SESSION["user_id"] = $user->user_id;
+    echo "$user->right_id";
+    echo "logged in";
     header("Location: ../");
 } else {
     die("You dont have permission to login");
